2210dd06982e8803f172d8f5210df2d061d08d8f
Kevin Li <kevinli617@gmail.com>
2023-07-13 10:26:50 -0400

    made landing page responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,16 @@
 import { Container } from "react-bootstrap";
 import bg from "../assets/bg.png";
 import bg2 from "../assets/bg2.png";
+import bg3 from "../assets/bg3.png";
 
 const Home = () => {
   return (
-    <Container className="w-100 mt-4">
-      <Container className="" style={{ zIndex: -1 }}>
+    <Container className="w-100 mt-4 mb-2">
+      <Container className="d-none d-md-block">
         <img src={bg2} className="w-100"></img>
+      </Container>
+      <Container className="d-block d-md-none">
+        <img src={bg3} className="w-100"></img>
       </Container>
       {/* <Container
         className="w-full d-flex flex-column align-items-center justify-content-center mx-auto text-center position-relative text-white"
