ae88230153122fff074d87c7a36fda8fb36f5368
Kevin Li <kevinli617@gmail.com>
2023-07-13 12:04:53 -0400

    optimized landing page responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import bg from "../assets/bg.png";
 import bg2 from "../assets/bg2.png";
 import bg3 from "../assets/bg3.png";
+import bg4 from "../assets/bg4.png";
 
 const Home = () => {
   return (
@@ -10,23 +11,31 @@ const Home = () => {
         <img src={bg2} className="w-100"></img>
       </Container>
       <Container className="d-block d-md-none">
-        <img src={bg3} className="w-100"></img>
+        <img src={bg4} className="w-100"></img>
       </Container>
       {/* <Container
         className="w-full d-flex flex-column align-items-center justify-content-center mx-auto text-center position-relative text-white"
-        style={{ height: "625px" }}
+        style={{ height: "460px" }}
       >
-        <span className="lead pb-2" style={{ textShadow: "2px 2px gray" }}>
-          <span className="display-1 fw-semibold mx-2">Kevin Li.</span>
+        <span
+          className="lead pb-4"
+          style={{ textShadow: "1px 1px gray", letterSpacing: 4 }}
+        >
+          <span className="display-3 fw-semibold mx-2">Kevin Li.</span>
           {"  "}
-          <span className="display-5 mx-2"> software engineer.</span>
+          <br />
+          <span className="fs-2 mx-2"> software engineer.</span>
         </span>
-        <span>
-          <h3 className="lead display-6 fw-medium">
-            Fueling Innovation <span className="display-6">and</span> Driving
-            Excellence
-            <br /> <span className="display-6">in</span> Today's Rapid Tech
-            Landscape.
+        <span style={{ letterSpacing: 3 }}>
+          <h3 className="lead fs-2 fw-medium">
+            Fueling Innovation{" "}
+            <span className="lead fs-3">
+              <br />
+              and
+            </span>{" "}
+            Driving Excellence
+            <br /> <span className=" lead fs-3">in</span> Today's Rapid <br />
+            Tech Landscape.
             <br />
           </h3>
         </span>
